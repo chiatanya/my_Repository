16673a77dc844b6f9527a3557c81aa014e1d61c3
chiatanya <52846996+chiatanya@users.noreply.github.com>
2021-09-02 12:06:36 +0530

    implimented sub in main

--- math.py
+++ math.py
@@ -3,7 +3,7 @@ def add(x,y):
     return x+y
 #subtract implimentation
 def subtract(x,y):
-    pass
+    return x-y               # from remote main
 #multiply implimentation
 def multiply(x,y):
     pass
